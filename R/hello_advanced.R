ddd6dae2c230eca98250b105c00bc7352cc085d5
alexnielson <alexnielson@utah.gov>
2020-06-23 12:30:33 -0600

    added a test function

---
+++ R/hello_advanced.R
@@ -0,0 +1,15 @@
+
+#' hello_advanced
+#'
+#' @param name A character string that we want the console to greet
+#'
+#' @return A string - the concatination of "Hello" + name + "!"
+#' @export
+#'
+#' @examples
+#' hello_advanced("Alexander")
+
+
+hello_advanced <- function(name){
+    print(paste0("Hello", name,"!"))
+}
